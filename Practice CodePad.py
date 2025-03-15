fee30f51c9b7abad20218bc0bdb434ae1f720d6a
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-15 16:08:15 -0500

    Practice CodePad

---
+++ Practice CodePad.py
@@ -0,0 +1,16 @@
+
+def your_code():
+
+    number = 8
+
+    remainder = number % 2
+
+    if remainder == 1:
+        return True
+    else:
+        return False
+    
+
+
+if __name__ == '__main__':
+    your_code()
