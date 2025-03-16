a097926b154280aeb379a00995de3f9ea96394af
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-15 19:09:16 -0500

    Exercises on Python Dictionary and Looping

---
+++ Practice Looping.py
@@ -0,0 +1,11 @@
+for number in range(20):
+    print(number)
+
+fav_foods = ["pizza", "tacos", "sushi", "burgers", "pasta"]
+
+for foods in fav_foods:
+    print(foods)
+
+for number in range(20):
+    print((number + 2) * 3)   
+                    
---
+++ Python Dictionary.py
@@ -0,0 +1,14 @@
+cats = {"Jane": 4, "John": 3, "Jill": 2, "Jack": 5}
+print(cats)
+
+cats["Ana"] = 7
+
+print(cats)
+
+len(cats)
+
+print(len(cats))
+
+del cats["Jane"]
+
+print(cats)
